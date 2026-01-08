490d59863fb81b161504a4e137cc2807faff212c
akram44121 <153254447+akram44121@users.noreply.github.com>
2026-01-08 15:01:06 +0100

    Racemblement du CSS

--- produits.php
+++ produits.php
@@ -4,30 +4,33 @@ session_start();
 <!DOCTYPE html>
 
 <html lang="fr">
+
 <head>
 	<title>TRUC</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="Front.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+		integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 
 <body>
-		
+
 	<!--*************** NAV ***************-->
 	<?php
-		require_once("view/nav.php");
+	require_once("view/nav.php");
 	?>
-	
-	<main id="container">
+
+	<main class="container-fluid py-4">
 	</main>
 
 	<!--*************** PIED DE PAGE ***************-->
 	<?php
-		require_once("view/footer.php");
+	require_once("view/footer.php");
 	?>
 
-	<!--*************** SCRIPT ***************-->
-	<script src="scripts/initListeProduits.js"></script>
+	<!--*************** SCRIPT 
+	<script src="scripts/initListeProduits.js"></script>***************-->
+
 </body>
 
 </html>
